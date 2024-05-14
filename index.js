54967fb48bc8e56f2891b8fbb3053352b78b6731
ZaidaLT01 <147778430+ZaidaLT01@users.noreply.github.com>
2024-05-14 08:58:28 -0400

    endpoint 5 y 6

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app= express();
 const PORT= 4000; //Pude cambiar 4500 u otro que no este siendo utilizado
 //array 
 let librosBiblicos=[
-{id: 1, nombre:'Genesis', autor:'Moises'},
-{id: 2, nombre:'Exodo', autor:'Moises'},
-{id: 3, nombre:'Levitico', autor:'Moises'}
+{id: 1, nombre:'Genesis', autor:'Moises',añoPublicación:2020},
+{id: 2, nombre:'Exodo', autor:'Moises',añoPublicación:2024},
+{id: 3, nombre:'Levitico', autor:'Moises',añoPublicación:1990}
 ];
 //manejo de JSON
 app.use(express.json());
@@ -45,8 +45,23 @@ app.put('/actualizar-libro/:id',(req,res)=>{
         res.status(404).json({mensaje:'Libro no Encontrado'});
     }
 });
-
-
+// endpoint 5 Eliminar
+app.delete('/eliminar-libro/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    lBiblico=librosBiblicos.filter(libro => libro.id !== id);
+    res.status(201).json({mensaje:'se ha eliminado el libro'});
+    console.log(lBiblico);
+});
+// endpoint 6 Año de publicacion
+app.get('/libros/publicacion/:anio',(req,res)=>{
+    const year = parseInt(req.params.anio);
+    const librosPublicados = librosBiblicos.filter(x => x.añoPublicación===year);
+    if(librosPublicados.length > 0 ){
+        res.json(librosPublicados);
+    }else{
+        res.status(404).json({mensaje:'no se han encontrado libros'});
+    }
+});
 
 app.listen(PORT,()=>{
     console.log("Servidor corriendo en el puerto http://localhost:"+PORT);
